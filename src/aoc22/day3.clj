bdb7f1e051584c52100c8c2281520c98f580c5fc
Kostas Georgiadis <kongeor@gmail.com>
2022-12-04 15:19:29 +0200

    day3...

--- src/aoc22/day3.clj
+++ src/aoc22/day3.clj
@@ -38,4 +38,28 @@
   (let [n (count e)
         [a b] (split-at (/ n 2) e)
         common (set/intersection (set a) (set b))]
-    ))
+    (reduce + (map letter-priority common))))
+
+(defn sum-of-priorities [items]
+  (reduce +
+          (map 
+           rearrangement-priority
+           items
+           )))
+
+(comment
+ (sum-of-priorities sample))
+
+;; 1
+(comment
+ (sum-of-priorities
+  (f/read-and-split "day3.txt")))
+
+;; 2
+(apply +
+       (map (fn [[a b c]]
+              (->
+               (set/intersection (set a) (set b) (set c))
+               first
+               letter-priority)) 
+            (partition 3 (f/read-and-split "day3.txt"))))
